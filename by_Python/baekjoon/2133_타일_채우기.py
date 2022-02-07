cac111a06dcede6cc80a73024b92d4772753b77d
03do-new30 <47556347+03do-new30@users.noreply.github.com>
2022-02-07 15:44:02 +0900

    백준: 2133_타일 채우기 [DP]

---
+++ by_Python/baekjoon/2133_타일_채우기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+dp = [0]*(n+1)
+dp[0] = 1
+
+for i in range(1, n+1):
+    if i % 2 == 1:
+        continue
+
+    dp[i] = dp[i-2]*3
+
+    for j in range(4, i+1):
+        if j % 2 == 0:
+            dp[i] += dp[i-j]*2
+
+print(dp[n])
