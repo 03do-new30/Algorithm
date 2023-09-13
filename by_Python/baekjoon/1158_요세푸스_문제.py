94ea953b79728af6168612a5dfea59ed8317c027
donew <eastgloss0330@naver.com>
2023-09-13 15:03:53 +0900

    [백준] 1158_요세푸스 문제

---
+++ by_Python/baekjoon/1158_요세푸스_문제.py
@@ -0,0 +1,19 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split(' '))
+q = deque()
+
+for i in range(1, n+1):
+    q.append(i)
+
+answer = []
+
+while q:
+    for i in range(k-1):
+        q.append(q.popleft())
+    answer.append(str(q.popleft()))
+
+print('<' + ', '.join(answer) + '>')
