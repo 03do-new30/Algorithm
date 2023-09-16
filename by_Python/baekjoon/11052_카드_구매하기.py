971d3faf6e806578fa98a206191ce25c8bee27c0
Donew <eastgloss0330@naver.com>
2023-09-16 15:57:25 +0900

    [백준] 11052_카드 구매하기 (재풀이)

--- by_Python/baekjoon/11052_카드_구매하기.py
+++ by_Python/baekjoon/11052_카드_구매하기.py
@@ -1,16 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
-p = [0] + list(map(int, input().split()))
+n = int(input())
+p = [0] + list(map(int, input().split(' ')))
 
-# dp[N] = 카드를 N개 살 때 지불해야하는 금액의 최대값
-# dp[N] = max(dp[N-1] + p[1], ..., dp[1] + p[N-1], dp[0] + ][N])
-dp = [0 for _ in range(n+1)]
+# dp[i] = 카드 i개를 갖기 위해 지불해야 하는 금액의 최대값
+dp = [0] * (n+1)
+
 for i in range(1, n+1):
-    if i == 1:
-        dp[1] = p[1]
-        continue
-    for j in range(1, i+1):
+    dp[i] = p[i]
+    for j in range(1, n+1):
+        if i <= j:
+            break
         dp[i] = max(dp[i], dp[i-j] + p[j])
+
 print(dp[n])
