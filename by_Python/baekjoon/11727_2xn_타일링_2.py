1c81c649f57e7b00f18004e9ceacf5cade83e006
Donew <eastgloss0330@naver.com>
2023-09-16 15:08:21 +0900

    [백준] 11727_2xn 타일링 2

---
+++ by_Python/baekjoon/11727_2xn_타일링_2.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+# dp[i] = 2*i 크기의 직사각형을 채우는 방법의 수
+dp = [0] * (n+1)
+
+for i in range(1, n+1):
+    if i == 1:
+        dp[i] = 1
+        continue
+    if i == 2:
+        dp[i] = 3
+        continue
+
+    dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007
+
+print(dp[n])
