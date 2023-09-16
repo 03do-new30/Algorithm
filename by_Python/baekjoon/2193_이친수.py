a7d648d1a021fe9faef35f76c4de22e98f467961
Donew <eastgloss0330@naver.com>
2023-09-16 17:38:38 +0900

    [백준] 2193_이친수 - 2차원 DP풀이

--- by_Python/baekjoon/2193_이친수.py
+++ by_Python/baekjoon/2193_이친수.py
@@ -1,19 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-"""
-이친수는 0으로 시작하지 않는다.
-이친수에서는 1이 두 번 연속으로 나타나지 않는다.
-(11을 부분 문자열로 갖지 않는다)
-"""
-n = int(input().strip())
-pinary = [0 for i in range(n+1)]
+n = int(input())
+
+# dp[i][0] = 마지막 수가 0인 i자리 이친수의 개수
+# dp[i][0] = dp[i-1][0] + dp[i-1][1]
+
+# dp[i][1] = 마지막 수가 1인 i자리 이친수의 개수
+# dp[i][1] = dp[i-1][0]
+
+dp = [[0] * 2 for _ in range(n+1)]
 
 for i in range(1, n+1):
     if i == 1:
-        pinary[1] = 1
-    elif i == 2:
-        pinary[2] = 1
-    else:
-        pinary[i] = pinary[i-2] + pinary[i-1]
-print(max(pinary))
+        dp[i][1] = 1
+        continue
+    dp[i][0] = dp[i-1][0] + dp[i-1][1]
+    dp[i][1] = dp[i-1][0]
+
+print(sum(dp[n]))
