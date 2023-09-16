2c109037161bd9a1671c63892b0b9d86ad36c984
Donew <eastgloss0330@naver.com>
2023-09-16 17:44:16 +0900

    [백준] 2193_이친수 - 1차원 DP풀이

--- by_Python/baekjoon/2193_이친수.py
+++ by_Python/baekjoon/2193_이친수.py
@@ -3,19 +3,16 @@ input = sys.stdin.readline
 
 n = int(input())
 
-# dp[i][0] = 마지막 수가 0인 i자리 이친수의 개수
-# dp[i][0] = dp[i-1][0] + dp[i-1][1]
+# dp[i] = i자리 이친수의 개수
+# 길이가 i인 이친수, 마지막 숫자가 0인 경우 = dp[i-1]
+# 길이가 i인 이친수, 마지막 숫자가 1인 경우 = dp[i-2]
+# dp[i] = dp[i-1] + dp[i-2]
 
-# dp[i][1] = 마지막 수가 1인 i자리 이친수의 개수
-# dp[i][1] = dp[i-1][0]
-
-dp = [[0] * 2 for _ in range(n+1)]
-
+dp = [0] * (n+1)
 for i in range(1, n+1):
     if i == 1:
-        dp[i][1] = 1
-        continue
-    dp[i][0] = dp[i-1][0] + dp[i-1][1]
-    dp[i][1] = dp[i-1][0]
+        dp[i] = 1
+    else:
+        dp[i] = dp[i-1] + dp[i-2]
 
-print(sum(dp[n]))
+print(dp[n])
