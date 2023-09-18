838d99081a4c7a490f2f7f3501ba61bd47439418
donew <eastgloss0330@naver.com>
2023-09-18 17:30:29 +0900

    1699_제곱수의 합

--- by_Python/baekjoon/1699_제곱수의_합.py
+++ by_Python/baekjoon/1699_제곱수의_합.py
@@ -2,38 +2,19 @@ import sys
 import math
 input = sys.stdin.readline
 
-n = int(input().strip())
+n = int(input())
 
-dp = [0]*(n+1)
+# dp[i] = i를 제곱수의 합으로 표현할 때 항의 최소 개수
+dp = [0] * (n+1)
+squares = [] # 제곱수들을 저장한다
 
-# bottom-up 방식
-
-
-def bottom_up(n):
-    # 제곱수는 1로 초기화
-    x = 1
-    while x*x <= n:
-        dp[x*x] = 1
-        x += 1
-
-    # n의 값이 0이 아니면(제곱수) 리턴
-    if dp[n] != 0:
-        return dp[n]
-
-    for i in range(1, n+1):
-        if dp[i] != 0:
-            continue
-
-        mini = i
-        # j*j <= i인 것 중
-        # dp[j*j] + dp[i-j*j]의 값이 최소인 것을
-        # dp[i]의 값으로 갱신
-        for j in range(1, int(math.sqrt(i)) + 1):
-            if 1 + dp[i-j*j] < mini:
-                mini = 1 + dp[i-j*j]  # 1 = dp[j*j]
-        dp[i] = mini
-
-    return dp[n]
-
-
-print(bottom_up(n))
+# dp[i] = 1 (i가 제곱수일 경우)
+for i in range(1, n + 1):
+    
+    dp[i] = i
+    j = 1
+    while j * j <= i:
+        dp[i] = min(dp[i], dp[i - j*j] + 1)
+        j += 1
+print(dp[n])
+ 
