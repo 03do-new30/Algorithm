20955c79e776752a3e8bca592c08c6e02224235f
donew <eastgloss0330@naver.com>
2023-09-18 15:31:10 +0900

    [백준] 1912_연속합

--- by_Python/baekjoon/1912_연속합.py
+++ by_Python/baekjoon/1912_연속합.py
@@ -1,15 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
-s = list(map(int, input().split()))
-dp = [-2000 for _ in range(n)]
+n = int(input())
+arr = list(map(int, input().split()))
 
+# dp[i] = arr[i]를 마지막으로 더할 때 연속된 수의 합의 최대
+dp = [0] * n
 for i in range(n):
     if i == 0:
-        dp[0] = s[0]
+        dp[i] = arr[i]
         continue
 
-    dp[i] = max(dp[i-1] + s[i], s[i])
+    dp[i]  = max(arr[i], dp[i-1] + arr[i])
 
 print(max(dp))
