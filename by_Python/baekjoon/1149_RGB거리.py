6a9f1cafa06244da9a0ad18dd196bb64eabd8e77
donew <eastgloss0330@naver.com>
2023-09-19 15:24:04 +0900

    [백준] 1149_RGB거리

--- by_Python/baekjoon/1149_RGB거리.py
+++ by_Python/baekjoon/1149_RGB거리.py
@@ -8,14 +8,16 @@ for _ in range(n):
     cost.append(list(map(int, input().split())))
 """input"""
 
-# 첫 번째 집이 각각 R, G, B일 경우에서 시작
-# 이전의 집과 겹치지 않는 색들의 최소값을 더해서 비용을 구해감
-for i in range(2, n+1):
-    # Red
-    cost[i][0] += min(cost[i-1][1], cost[i-1][2])
-    # Green
-    cost[i][1] += min(cost[i-1][0], cost[i-1][2])
-    # Blue
-    cost[i][2] += min(cost[i-1][0], cost[i-1][1])
+# dp[i][color] = i번 집이 color 색상으로 집을 칠했을 때, 
+#                1 ~ i번 집을 칠하는 데 드는 총 최소 비용
+dp = [[0] * 3 for _ in range(n+1)]
+for i in range(1, n+1):
+    for color in range(n):
+        if color == 0:
+            dp[i][color] = min(dp[i-1][1], dp[i-1][2]) + cost[i][color]
+        elif color == 1:
+            dp[i][color] = min(dp[i-1][0], dp[i-1][2]) + cost[i][color]
+        elif color == 2:
+            dp[i][color] = min(dp[i-1][0], dp[i-1][1]) + cost[i][color]
 
-print(min(cost[n]))
+print(min(dp[n]))
