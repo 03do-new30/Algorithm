4efcd779a5d7babe3c2d1d644d06d645af8714f2
Donew <eastgloss0330@naver.com>
2023-09-21 23:59:52 +0900

    [백준] 2156_포도주 시식

--- by_Python/baekjoon/2156_포도주_시식.py
+++ by_Python/baekjoon/2156_포도주_시식.py
@@ -2,19 +2,27 @@ import sys
 input = sys.stdin.readline
 
 n = int(input().strip())
-wine = [int(input().strip()) for _ in range(n)]
+wine = [0] + [int(input().strip()) for _ in range(n)]
 
 # dp[i] = i번째 잔까지 왔을 때, 마실 수 있는 최대 양
-dp = [0 for _ in range(n)]
+dp = [0 for _ in range(n+1)]
 
-for i in range(n):
-    if i == 0:
-        dp[0] = wine[0]
-        continue
+# dp[i] = max(dp[i-1], dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i])
+for i in range(1, n+1):
     if i == 1:
-        dp[1] = wine[0] + wine[1]
+        dp[i] = wine[i]
         continue
-    dp[i] = max(dp[i-2] + wine[i],
-                dp[i-3] + wine[i-1] + wine[i],
-                dp[i-1])
+    if i == 2:
+        dp[i] = wine[i] + wine[i-1]
+        continue
+
+    if i - 1 >= 0:
+        dp[i] = dp[i-1]
+
+    if i - 2 >= 0:
+        dp[i] = max(dp[i], dp[i-2] + wine[i])
+
+    if i - 3 >= 0:
+        dp[i] = max(dp[i], dp[i-3] + wine[i-1] + wine[i])
+
 print(max(dp))
