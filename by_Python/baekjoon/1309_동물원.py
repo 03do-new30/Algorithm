a82efa901dfa77ca30cbe4b3ea46505fc947fdea
donew <eastgloss0330@naver.com>
2023-09-21 14:55:06 +0900

    [백준] 1309_동물원

--- by_Python/baekjoon/1309_동물원.py
+++ by_Python/baekjoon/1309_동물원.py
@@ -3,20 +3,17 @@ input = sys.stdin.readline
 
 n = int(input().strip())
 
-# dp[n][0] = n번째 줄에 사자가 없음
-# dp[n][1] = n번째 줄의 왼쪽에 사자 배치
-# dp[n][2] = n번째 줄의 오른쪽에 사자 배치
-dp = [[0, 0, 0] for _ in range(n+1)]
+# dp[i][0] = i번째 행에 사자를 배치하지 않는 경우의수 = dp[i-1][0] + dp[i-1][1] + dp[i-1][2]
+# dp[i][1] = i번째 행 첫번째 칸에 사자를 배치하는 경우의 수 = dp[i-1][0] + dp[i-1][2] = dp[i][0] - dp[i-1][1]
+# dp[i][2] = i번째 행 두번째 칸에 사자를 배치하는 경우의 수 = dp[i-1][0] + dp[i-1][1] = dp[i][0] - dp[i-1][2]
+dp = [[0] * 3 for _ in range(n+1)]
+dp[0][0] = 1
+
+mod = 9901
 
 for i in range(1, n+1):
-    if i == 1:
-        dp[1][0] = 1
-        dp[1][1] = 1
-        dp[1][2] = 1
-    else:
-        dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % 9901
-        dp[i][1] = dp[i-1][0] + dp[i-1][2] % 9901
-        dp[i][2] = dp[i-1][0] + dp[i-1][1] % 9901
+    dp[i][0] = sum(dp[i-1]) % mod
+    dp[i][1] = (dp[i][0] - dp[i-1][1]) % mod
+    dp[i][2] = (dp[i][0] - dp[i-1][2]) % mod
 
-# dp[n]의 합이 9901을 초과할 수 있으므로 마지막에 한 번 더 연산!!!
-print(sum(dp[n]) % 9901)
+print(sum(dp[n]) % mod)
