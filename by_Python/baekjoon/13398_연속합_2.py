f88675fe363b4c6925337126b3dba45e0dda8e36
Donew <eastgloss0330@naver.com>
2023-09-22 21:16:32 +0900

    [백준] 13398_연속합 2

--- by_Python/baekjoon/13398_연속합_2.py
+++ by_Python/baekjoon/13398_연속합_2.py
@@ -7,9 +7,18 @@ a = [0] + list(map(int, input().split()))
 # dp[0][i] = 중간에 제거된 항 없이 +a[i]를 할 때 연속합 max
 # dp[1][i] = 중간에 제거된 항 존재 +a[i]를 할 때 연속합 max
 
-dp = [[0] * (n+1) for _ in range(2)]
+# dp[0][i] = max(dp[0][i-1] + a[i], a[i])
+# dp[1][i] = max(dp[1][i-1] + a[i], dp[0][i-2] + a[i])
+
+dp = [[-10000] * (n+1) for _ in range(2)]
 
 for i in range(1, n+1):
     if i == 1:
         dp[0][i] = a[i]
         dp[1][i] = a[i]
+        continue
+
+    dp[0][i] = max(dp[0][i-1] + a[i], a[i])
+    dp[1][i] = max(dp[1][i-1] + a[i], dp[0][i-2] + a[i])
+
+print(max(map(max, dp)))
