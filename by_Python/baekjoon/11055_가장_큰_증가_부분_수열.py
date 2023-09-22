969d31f18eff2a388a51da7a519e9c44f5b5e9bf
donew <eastgloss0330@naver.com>
2023-09-22 14:07:03 +0900

    [백준] 11055_가장 큰 증가하는 부분 수열

--- by_Python/baekjoon/11055_가장_큰_증가_부분_수열.py
+++ by_Python/baekjoon/11055_가장_큰_증가_부분_수열.py
@@ -1,25 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
+n = int(input())
 a = [0] + list(map(int, input().split()))
 
-# dp[x] = x번 인덱스까지의 가장 큰 부분수열의 합
-# x번 인덱스까지의 가장 "작은" 부분수열의 합은 a[x]일 것! (길이가 1)
-# dp[x] = a[x]로 초기화한다
-dp = a[:]
+# dp[i] = a[i]가 증가하는 부분 수열의 마지막 수일 때, 수열 합의 최대
+# dp[i] = max(dp[j] (0 <= j < i and a[j] < a[i])) + a[i]
+dp = [0] * (n+1)
 
 for i in range(1, n+1):
-    #print(i, "번 인덱스까지의 가장 큰 부분수열의 합 구하기")
-    for j in range(1, i):
-        if a[j] < a[i]:  # 증가하는 수열일 때
-            #print(j, "번 인덱스 뒤에", i, "번 인덱스가 올 때 합은", dp[j] + a[i])
-            dp[i] = max(dp[i], dp[j] + a[i])
-    # print(dp)
-    # print('-'*20)
-print(max(dp))
+    for j in range(i):
+        if a[j] < a[i]:
+            dp[i] = max(dp[i], dp[j])
+    dp[i] += a[i]
 
-# 반례
-# 8
-# 3 10 2 7 11 5 13 8
-# 3 + 10 + 11 + 13 = 37이 되어야 함
+print(max(dp))
