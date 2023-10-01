dc6621340e46d039d21fec794727b2f3ed0b60bc
Donew <eastgloss0330@naver.com>
2023-10-01 15:48:55 +0900

    [백준] 3085_사탕 게임

--- by_Python/baekjoon/3085_사탕_게임.py
+++ by_Python/baekjoon/3085_사탕_게임.py
@@ -1,65 +1,61 @@
 import sys
 input = sys.stdin.readline
 
+n = int(input())
+arr = [list(input().strip()) for _ in range(n)]
 
-def count(arr):
-    # 가장 긴 행 또는 열의 연속 부분을 구한다
-    max_cnt = 0
 
-    # 행
-    for row in range(N):
-        prev = arr[row][0]
-        cnt = 1
-        for col in range(1, N):
-            if arr[row][col] == prev:
-                cnt += 1
+### 1 ###
+# 모두 같은색으로 이루어진 가장 긴 연속 부분 찾기
+def count_candy():
+    ret = 0
+    # 행 검사
+    for r in range(n):
+        row_cnt = 0
+        for c in range(n):
+            if c == 0:
+                row_cnt = 1
+                continue
+            if arr[r][c] == arr[r][c-1]:
+                row_cnt += 1
             else:
-                cnt = 1  # reset
-                prev = arr[row][col]
-
-            # 현재까지의 cnt가 max_cnt보다 크면 갱신
-            max_cnt = max(max_cnt, cnt)
-
-    # 열
-    for col in range(N):
-        prev = arr[0][col]
-        cnt = 1
-        for row in range(1, N):
-            if arr[row][col] == prev:
-                cnt += 1
+                ret = max(ret, row_cnt)
+                row_cnt = 1
+        ret = max(ret, row_cnt)
+    # 열 검사
+    for c in range(n):
+        col_cnt = 0
+        for r in range(n):
+            if r == 0:
+                col_cnt = 1
+                continue
+            if arr[r][c] == arr[r-1][c]:
+                col_cnt += 1
             else:
-                cnt = 1  # reset
-                prev = arr[row][col]
+                ret = max(ret, col_cnt)
+                col_cnt = 1
+        ret = max(ret, col_cnt)
+    
+    return ret
+            
+### 2 ###
+# 인접한 칸 -> 각 칸의 우측 & 하단만 검사해줘도 가능
+dirs = [(0, 1), (1, 0)]
 
-            # 현재까지의 cnt가 max_cnt보다 크면 갱신
-            max_cnt = max(max_cnt, cnt)
+### 3 ###
+max_candy = 0
+for r in range(n):
+    for c in range(n):
+        for x, y in dirs:
+            new_r = r + x
+            new_c = c + y
+            if 0 <= new_r < n and 0 <= new_c < n:
+                if arr[r][c] != arr[new_r][new_c]:
+                    # swap
+                    arr[r][c], arr[new_r][new_c] = arr[new_r][new_c], arr[r][c]
+                    # count
+                    max_candy = max(max_candy, count_candy())
+                    # re-swap
+                    arr[r][c], arr[new_r][new_c] = arr[new_r][new_c], arr[r][c]
 
-    return max_cnt
-
-
-N = int(input().strip())
-arr = [list(input().strip()) for _ in range(N)]  # 사탕이 채워진 상태
-
-# 아래쪽, 오른쪽만 검사하면 된다
-# e.g., (r, c)와 그 우측을 교체한 것 = (r, c+1)과 그 좌측을 교체한 것
-dr = [0, 1]
-dc = [1, 0]
-
-max_ans = 0
-
-for r in range(N):
-    for c in range(N):
-        for i in range(2):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < N and 0 <= nc < N:
-                # 사탕의 색이 다른 인접한 두 칸을 고른다
-                if arr[r][c] != arr[nr][nc]:
-                    # 교체
-                    arr[r][c], arr[nr][nc] = arr[nr][nc], arr[r][c]
-                    # 카운트
-                    max_ans = max(max_ans, count(arr))
-                    # 교체
-                    arr[r][c], arr[nr][nc] = arr[nr][nc], arr[r][c]
-
-print(max_ans)
+print(max_candy)
