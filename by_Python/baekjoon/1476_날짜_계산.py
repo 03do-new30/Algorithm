30c4f8f9c81cdd09efd3a4db98a25bac858305e2
Donew <eastgloss0330@naver.com>
2023-10-01 16:24:00 +0900

    [백준] 1476_날짜 계산 - 나머지 연산 활용

--- by_Python/baekjoon/1476_날짜_계산.py
+++ by_Python/baekjoon/1476_날짜_계산.py
@@ -3,20 +3,16 @@ input = sys.stdin.readline
 
 E, S, M = map(int, input().split())
 
-# 하나씩 해보기
-cnt = 1
-e = 1
-s = 1
-m = 1
-while e != E or s != S or m != M:
-    e += 1
-    s += 1
-    m += 1
-    cnt += 1
-    if e > 15:
-        e -= 15
-    if s > 28:
-        s -= 28
-    if m > 19:
-        m -= 19
-print(cnt)
+# 모든 E, S, M에서 1을 빼면
+# year % 15 == E
+# year % 28 == S
+# year % 19 == M
+# 인 year를 찾는 문제가 된다
+E -= 1
+S -= 1
+M -= 1
+
+for year in range(7981):
+    if year % 15 == E and year % 28 == S and year % 19 == M:
+        print(year + 1)
+        break
