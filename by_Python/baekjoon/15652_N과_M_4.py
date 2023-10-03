5dcab54b99bca232193cf94c587a142963c277a3
03do-new30 <47556347+03do-new30@users.noreply.github.com>
2023-10-03 18:41:04 +0900

    [백준] 15652_N과 M (4) - 선택의 관점

--- by_Python/baekjoon/15652_N과_M_4.py
+++ by_Python/baekjoon/15652_N과_M_4.py
@@ -2,17 +2,33 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-seq = [-1] * m  # 비내림차순 순열 저장
+cnt = [0] * (n+1)  # cnt[i] = 숫자 i가 몇번 등장하는지 저장
+
+# num - 포함할지 말지 결정해야 할 숫자
+# selected - 현재까지 선택한 수의 개수
 
 
-def solve(idx, start):
-    if idx == m:
-        print(' '.join(list(map(str, seq))))
+def solve(num, selected):  # 선택의 관점
+    if selected == m:
+        # cnt에 저장된 등장 횟수를 이용하여 출력한다
+        seq = []
+        for i in range(1, n+1):
+            for j in range(cnt[i]):
+                seq.append(str(i))
+        print(' '.join(seq))
         return
 
-    for num in range(start, n+1):
-        seq[idx] = num
-        solve(idx + 1, num)
+    if num > n:
+        return
+
+    # 선택하는 경우
+    # 몇 개를 중복으로 선택할건지
+    for i in range(m-selected, 0, -1):  # 사전순으로 출력하기 위한 순서
+        cnt[num] = i
+        solve(num + 1, selected + i)
+    # 선택하지 않는 경우
+    cnt[num] = 0
+    solve(num + 1, selected)
 
 
-solve(0, 1)
+solve(1, 0)
