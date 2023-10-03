9d3deebba61c8231b62449f01d9ab09ec36c81c6
03do-new30 <47556347+03do-new30@users.noreply.github.com>
2023-10-03 17:42:42 +0900

    [백준] 15649_N과 M (1) - 인덱스를 재귀함수 파라미터로

--- by_Python/baekjoon/15649_N과_M_1.py
+++ by_Python/baekjoon/15649_N과_M_1.py
@@ -2,22 +2,24 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
+seq = [-1] * (m+1)  # 수열 저장
+visited = [False] * (n+1)
 
 
-def solve(seq, visited):
-    if len(seq) == m:
-        print(' '.join(list(map(str, seq))))
+def solve(idx):
+    if idx == m+1:  # 수열의 길이가 m이 되면 출력
+        print(' '.join(list(map(str, seq[1:]))))
         return
 
-    for i in range(1, n+1):
-        if not visited[i]:
-            new_seq = seq + [i]
-            visited[i] = True
-            solve(new_seq, visited)
-            visited[i] = False
+    # idx번째에 수를 추가
+    for num in range(1, n+1):
+        if visited[num]:
+            continue
+        seq[idx] = num
+        visited[num] = True
+        solve(idx + 1)
+        visited[num] = False
 
 
-for i in range(1, n+1):
-    v = [False] * (n+1)
-    v[i] = True
-    solve([i], v)
+visited[0] = True
+solve(1)
