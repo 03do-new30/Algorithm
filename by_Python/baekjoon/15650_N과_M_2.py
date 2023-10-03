8e16d590ff9ce7b8f85471bad3d3f08cbec83dc9
03do-new30 <47556347+03do-new30@users.noreply.github.com>
2023-10-03 17:57:03 +0900

    [Refactor][백준] 15650_N과 M (2) - visited 배열 삭제 및 start 파라미 추가

--- by_Python/baekjoon/15650_N과_M_2.py
+++ by_Python/baekjoon/15650_N과_M_2.py
@@ -3,23 +3,17 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-visited = [False] * (n+1)  # 방문 여부 저장
 seq = [-1] * m  # 수열 저장
 
 
-def solve(idx):
+def solve(idx, start):
     if idx == m:
         print(' '.join(list(map(str, seq))))
         return
 
-    for num in range(1, n+1):
-        if visited[num]:
-            continue
-        if (idx > 0 and seq[idx - 1] < num) or (idx == 0):
-            visited[num] = True
-            seq[idx] = num
-            solve(idx + 1)
-            visited[num] = False
+    for num in range(start, n+1):
+        seq[idx] = num
+        solve(idx + 1, num + 1)
 
 
-solve(0)
+solve(0, 1)
