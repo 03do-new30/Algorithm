16dc44a0b2786c73e663a8925627633c0b4a83af
donew <eastgloss0330@naver.com>
2023-10-06 13:21:26 +0900

    [백준] 10973_이전 순열

--- by_Python/baekjoon/10973_이전_순열.py
+++ by_Python/baekjoon/10973_이전_순열.py
@@ -1,17 +1,30 @@
-# 출처: https://ji-gwang.tistory.com/264
 import sys
 input = sys.stdin.readline
 
-N = int(input())
+n = int(input())
 seq = list(map(int, input().split()))
 
-for i in range(N-1, 0, -1):
+# 이전 순열
+# 주어진 순열이 어떤 숫자들로 시작하는 순열의 첫번째 순열인지 판별
+# 그 이전 순열은 또 다른 어떤 숫자들로 시작하는 순열의 마지막 순열
+
+### 1 ###
+# seq[i-1] > seq[i]인 가장 큰 i를 찾는다
+for i in range(n-1, -1, -1):
+    if i == 0:
+        print(-1)
+        break
+    
     if seq[i-1] > seq[i]:
-        for j in range(N-1, 0, -1):
+        ### 2 ###
+        # seq[i-1]보다 작은 seq[j] (i <= j < n) 중 가장 큰 seq[j] 를 찾아 swap
+        for j in range(n-1, i-1, -1):
             if seq[i-1] > seq[j]:
                 seq[i-1], seq[j] = seq[j], seq[i-1]
-                seq = seq[:i] + sorted(seq[i:], reverse=True)
-                print(*seq)
-                exit()
+                break
+        ### 3 ###
+        # 이전 순열은 seq[:i] + reversed(seq[i:])
+        seq = seq[:i] + list(reversed(seq[i:]))
 
-print(-1)
+        print(' '.join(list(map(str, seq))))
+        break
