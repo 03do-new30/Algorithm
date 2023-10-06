0a4d851fadd88ee0d05062a93b560269fc148cda
donew <eastgloss0330@naver.com>
2023-10-06 17:33:06 +0900

    [백준] 10973_이전  순열 - prev_permutation 함수 만들어서 사용

--- by_Python/baekjoon/10973_이전_순열.py
+++ by_Python/baekjoon/10973_이전_순열.py
@@ -1,30 +1,28 @@
 import sys
 input = sys.stdin.readline
 
+def prev_permutation(seq):
+    i = len(seq) - 1
+    while i > 0 and seq[i-1] <= seq[i]:
+        i -= 1
+    if i == 0:
+        return False
+    
+    j = len(seq) - 1
+    while seq[i-1] <= seq[j]:
+        j -= 1
+    seq[i-1], seq[j] = seq[j], seq[i-1]
+
+    j = len(seq) - 1
+    while i < j:
+        seq[i], seq[j] = seq[j], seq[i]
+        i += 1
+        j -= 1
+    return True
+
 n = int(input())
 seq = list(map(int, input().split()))
-
-# 이전 순열
-# 주어진 순열이 어떤 숫자들로 시작하는 순열의 첫번째 순열인지 판별
-# 그 이전 순열은 또 다른 어떤 숫자들로 시작하는 순열의 마지막 순열
-
-### 1 ###
-# seq[i-1] > seq[i]인 가장 큰 i를 찾는다
-for i in range(n-1, -1, -1):
-    if i == 0:
-        print(-1)
-        break
-    
-    if seq[i-1] > seq[i]:
-        ### 2 ###
-        # seq[i-1]보다 작은 seq[j] (i <= j < n) 중 가장 큰 seq[j] 를 찾아 swap
-        for j in range(n-1, i-1, -1):
-            if seq[i-1] > seq[j]:
-                seq[i-1], seq[j] = seq[j], seq[i-1]
-                break
-        ### 3 ###
-        # 이전 순열은 seq[:i] + reversed(seq[i:])
-        seq = seq[:i] + list(reversed(seq[i:]))
-
-        print(' '.join(list(map(str, seq))))
-        break
+if prev_permutation(seq):
+    print(' '.join(list(map(str, seq))))
+else:
+    print(-1)
