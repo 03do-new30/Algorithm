77210df8d37133160ee35584002d7bc7f4c13343
donew <eastgloss0330@naver.com>
2023-10-06 17:47:38 +0900

    [백준] 10819_차이를 최대로  - next_permutation 함수 만들어서 사용

--- by_Python/baekjoon/10819_차이를_최대로.py
+++ by_Python/baekjoon/10819_차이를_최대로.py
@@ -1,34 +1,42 @@
 import sys
 input = sys.stdin.readline
 
+def next_permutation(a):
+    i = len(a) - 1
+    while i > 0 and a[i-1] >= a[i]:
+        i -= 1
+    if i == 0:
+        return False
+    
+    j = len(a) - 1
+    while a[j] <= a[i-1]:
+        j -= 1
+    
+    a[i-1], a[j] = a[j], a[i-1]
+
+    j = len(a) - 1
+    while i < j:
+        a[i], a[j] = a[j], a[i]
+        j -= 1
+        i += 1
+    return True
+
+def calc(a):
+    ret = 0
+    for i in range(1, len(a)):
+        ret += abs(a[i] - a[i-1])
+    return ret
+
 n = int(input())
 a = list(map(int, input().split()))
+a.sort()
 
 # 주어진 정수들로 순열을 만들어보며 하나하나 최댓값 구함
 # N! = 8! = 40320
 # N * N!
-ans = -100
-seq = [0] * n
-visited = [False] * n
-def solve(idx):
-    global ans
-
-    if idx == n:
-        tmp = 0
-        for i in range(n-1):
-            tmp += abs(seq[i] - seq[i+1])
-        
-        if ans < tmp:
-            ans = tmp
-        return
-    
-    for i in range(n):
-        if visited[i]:
-            continue
-        visited[i] = True
-        seq[idx] = a[i]
-        solve(idx + 1)
-        visited[i] = False
-
-solve(0)
-print(ans)
+answer = -100
+while True:
+    answer = max(answer, calc(a))
+    if not next_permutation(a):
+        break
+print(answer)
