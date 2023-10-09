352044104ee197f936304962bf728f64373d7e73
donew <eastgloss0330@naver.com>
2023-10-09 14:22:21 +0900

    [백준] 14889_스타트와 링크 - 재귀

--- by_Python/baekjoon/14889_스타트와_링크.py
+++ by_Python/baekjoon/14889_스타트와_링크.py
@@ -4,52 +4,39 @@ input = sys.stdin.readline
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-def next_permutation(a):
-    i = len(a) - 1
-    while i > 0 and a[i-1] >= a[i]:
-        i -= 1
-    if i == 0:
-        return False
+# idx번째 사람을 어떤 팀에 넣을지 결정한다
+def solve(idx, team_a, team_b):
+    if idx == n:
+        if len(team_a) != n // 2:
+            return -1
+        if len(team_b) != n // 2:
+            return -1
+        
+        stat_a = 0
+        stat_b = 0
+        
+        for i in range(n // 2 - 1):
+            for j in range(i, n // 2):
+                stat_a += arr[team_a[i]][team_a[j]] + arr[team_a[j]][team_a[i]]
+                stat_b += arr[team_b[i]][team_b[j]] + arr[team_b[j]][team_b[i]]
+        
+        diff = abs(stat_a - stat_b)
+        return diff
     
-    j = len(a) - 1
-    while a[j] <= a[i-1]:
-        j -= 1
-    a[i-1], a[j] = a[j], a[i-1]
-
-    j = len(a) - 1
-    while i < j:
-        a[i], a[j] = a[j], a[i]
-        i += 1
-        j -= 1
-    return True
-
-def stat(team):
-    ret = 0
-    for i in range(len(team) -1):
-        for j in range(i+1, len(team)):
-            x = team[i]
-            y = team[j]
-            ret += arr[x][y] + arr[y][x]
-    return ret
-
-seq = [0] * (n//2) + [1] * (n//2)
-answer = 1000000
-while True:
-    team_a = []
-    team_b = []
-    for i in range(n):
-        if seq[i] == 0:
-            team_a.append(i)
+    answer = -1
+    
+    # a팀
+    tmp = solve(idx + 1, team_a + [idx], team_b)
+    if tmp != -1:
+        answer = tmp
+    # b팀
+    tmp = solve(idx + 1, team_a, team_b + [idx])
+    if tmp != -1:
+        if answer != -1:
+            answer = min(answer ,tmp)
         else:
-            team_b.append(i)
-    # print("team_a:", team_a, "team_b:", team_b)
-    stat_a = stat(team_a)
-    stat_b = stat(team_b)
-    # print("stat_a:", stat_a, "stat_b:", stat_b)
-    diff = abs(stat_a - stat_b)
-    answer = min(answer, diff)
-    if answer == 0:
-        break
-    if not next_permutation(seq):
-        break
-print(answer)
+            answer = tmp
+    
+    return answer
+
+print(solve(0, [], []))
