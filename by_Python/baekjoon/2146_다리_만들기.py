4e4cf05c0f3a432986bb5759468479acede054b7
donew <eastgloss0330@naver.com>
2023-10-15 23:58:16 +0900

    [백준] 2146_다리 만들기 - 내 풀이

--- by_Python/baekjoon/2146_다리_만들기.py
+++ by_Python/baekjoon/2146_다리_만들기.py
@@ -1,75 +1,89 @@
-# https://kyun2da.github.io/2021/04/22/makeBridge/
-from collections import deque
 import sys
 input = sys.stdin.readline
+from collections import deque
 
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-def mark_island(r, c, num):
-    arr[r][c] = num
+# 섬 표시
+island = 0
+visited = [[False] * n for _ in range(n)]
+dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+def bfs_island(r, c):
     q = deque([(r, c)])
-
+    visited[r][c] = True
+    arr[r][c] += island
     while q:
         r, c = q.popleft()
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < N and 0 <= nc < N:
-                if arr[nr][nc] > 0 and not visited[nr][nc]:
-                    arr[nr][nc] = num
-                    q.append((nr, nc))
-                    visited[nr][nc] = True
+        for dir in dirs:
+            new_r = r + dir[0]
+            new_c = c + dir[1]
+            if 0 <= new_r < n and 0 <= new_c < n:
+                if arr[new_r][new_c] == 1 and not visited[new_r][new_c]:
+                    visited[new_r][new_c] = True
+                    arr[new_r][new_c] += island
+                    q.append((new_r, new_c))
 
+for r in range(n):
+    for c in range(n):
+        if not visited[r][c] and arr[r][c] == 1:
+            bfs_island(r, c)
+            island += 1
 
-def make_bridge(num):
-    global ans
+# bfs를 마친 후 현재 섬의 총 개수는 island 개
 
-    dist = [[-1]*N for _ in range(N)]  # 거리가 저장되는 배열
+# 다른 섬으로 도착하는 최단거리를 찾는다
+# 각 섬에서 물가에 있는 좌표들을 BFS 시작 지점으로 삼는다
+start_points = dict()
+for r in range(n):
+    for c in range(n):
+        if arr[r][c] == 0:
+            continue
+        
+        island_sign = arr[r][c]
+        # 물가에 있는지 검사한다
+        for dir in dirs:
+            new_r = r + dir[0]
+            new_c = c + dir[1]
+            if 0 <= new_r < n and 0 <= new_c < n:
+                if arr[new_r][new_c] == 0:
+                    # 물가에 있음
+                    if island_sign in start_points:
+                        start_points[island_sign].append((r, c))
+                    else:
+                        start_points[island_sign] = [(r, c)]
+                    # 물가에 있는 것을 알았으므로 종료
+                    break
+
+# print("start_points", start_points)
+
+shortest = 100000
+def bfs_bridge(island_sign):
+    
+    global shortest
+
     q = deque()
+    visited = [[False] * n for _ in range(n)]
 
-    for r in range(N):
-        for c in range(N):
-            # 번호 num인 섬에 속하는 좌표들을 q에 삽입
-            if arr[r][c] == num:
-                q.append((r, c))
-                dist[r][c] = 0
-
+    points = start_points[island_sign]
+    for r, c in points:
+        q.append((r, c, 0))
+        visited[r][c] = True
+    
     while q:
-        r, c = q.popleft()
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < N and 0 <= nc < N:
-                # 다른 섬
-                if arr[nr][nc] > 0 and arr[nr][nc] != num:
-                    ans = min(ans, dist[r][c])
-                    return
-                # 바다
-                if arr[nr][nc] == 0 and dist[nr][nc] == -1:
-                    dist[nr][nc] = dist[r][c] + 1
-                    q.append((nr, nc))
+        r, c, bridge = q.popleft()
+        for dir in dirs:
+            new_r = r + dir[0]
+            new_c = c + dir[1]
+            if 0 <= new_r < n and 0 <= new_c < n and not visited[new_r][new_c]:
+                if arr[new_r][new_c] == 0:
+                    q.append((new_r, new_c, bridge + 1))
+                    visited[new_r][new_c] = True
+                elif arr[new_r][new_c] != island_sign:
+                    shortest = min(shortest, bridge)
+                    visited[new_r][new_c] = True
 
+for i in range(1, island + 1):
+    bfs_bridge(i)
 
-N = int(input().strip())
-arr = [list(map(int, input().split())) for _ in range(N)]
-
-dr = [0, 0, -1, 1]
-dc = [-1, 1, 0, 0]
-
-# 가장 짧은 다리의 길이
-ans = 10000
-
-# 섬에 번호 매기기
-visited = [[False]*N for _ in range(N)]
-island_num = 1
-for r in range(N):
-    for c in range(N):
-        # 섬을 1, 2, 3... 번호 매긴다
-        if arr[r][c] == 1 and not visited[r][c]:
-            mark_island(r, c, island_num)
-            island_num += 1
-
-# 다리 놓기
-for i in range(1, island_num):
-    make_bridge(i)
-
-print(ans)
+print(shortest)
