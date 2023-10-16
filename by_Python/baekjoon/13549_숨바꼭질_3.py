a02a36c2e327aed8b9c53ade55649e49365cfaa7
donew <eastgloss0330@naver.com>
2023-10-16 16:49:12 +0900

    [백준] 13549_숨바꼭질 3 - 큐를 현재큐와 다음큐로 나누어 풀이

--- by_Python/baekjoon/13549_숨바꼭질_3.py
+++ by_Python/baekjoon/13549_숨바꼭질_3.py
@@ -2,34 +2,32 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
+n, k = map(int, input().split())
 
-visited = [-1]*100001
-visited[N] = 0
+max = 100001
+visited = [-1] * max
+current_q = deque() # 시간이 0
+next_q = deque() # 시간이 1
 
-q = deque([N])
-while q:
-    x = q.popleft()
+visited[n] = 0
+current_q.append(n)
 
-    if x == K:
-        print(visited[x])
-        break
-
-    """
-    반례: 1 2가 입력으로 주어졌을 때
-    0이 출력되어야하는데,
-    x*2일 때의 검사를 마지막으로 해주면
-    visited[x+1]이 먼저 갱신되어서 답이 1로 나온다!
-    x*2를 먼저 검사해준다.
-    """
-    if 0 <= x * 2 < 100001 and visited[x*2] == -1:
-        visited[x*2] = visited[x]
-        # 0 - 1 BFS
-        # 가중치가 0인 것은 큐의 뒤가 아니라 앞에 넣는다
-        q.appendleft(x*2)
-    if 0 <= x - 1 < 100001 and visited[x-1] == -1:
+while current_q:
+    x = current_q.popleft()
+    # 순간이동
+    if x * 2 < max and visited[x * 2] == -1:
+        visited[x * 2] = visited[x]
+        current_q.append(x * 2)
+    # 걷기
+    if 0 <=  x - 1 and visited[x-1] == -1:
         visited[x-1] = visited[x] + 1
-        q.append(x-1)
-    if 0 <= x + 1 < 100001 and visited[x+1] == -1:
-        visited[x+1] = visited[x] + 1
-        q.append(x+1)
+        next_q.append(x-1)
+    if x + 1 < max and visited[x + 1] == -1:
+        visited[x + 1] = visited[x] + 1
+        next_q.append(x + 1)
+    
+    if len(current_q) == 0:
+        current_q = next_q
+        next_q = deque()
+
+print(visited[k])
