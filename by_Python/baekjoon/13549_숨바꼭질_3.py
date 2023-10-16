5226bba2c78e08ef4b19427f98778bd05c8005d7
donew <eastgloss0330@naver.com>
2023-10-16 16:53:01 +0900

    [백준] 13549_숨바꼭질 3 - 덱을 사용해 순간이동은 덱 앞으로, 걷기는 덱 뒤로 넣는 방법

--- by_Python/baekjoon/13549_숨바꼭질_3.py
+++ by_Python/baekjoon/13549_숨바꼭질_3.py
@@ -6,28 +6,25 @@ n, k = map(int, input().split())
 
 max = 100001
 visited = [-1] * max
-current_q = deque() # 시간이 0
-next_q = deque() # 시간이 1
+q = deque()
 
 visited[n] = 0
-current_q.append(n)
+q.append(n)
 
-while current_q:
-    x = current_q.popleft()
+# 순간이동할 경우에는 큐의 앞에,
+# 걸어서 이동하는 경우에는 큐의 뒤에
+
+while q:
+    x = q.popleft()
     # 순간이동
     if x * 2 < max and visited[x * 2] == -1:
         visited[x * 2] = visited[x]
-        current_q.append(x * 2)
+        q.appendleft(x * 2)
     # 걷기
     if 0 <=  x - 1 and visited[x-1] == -1:
         visited[x-1] = visited[x] + 1
-        next_q.append(x-1)
+        q.append(x-1)
     if x + 1 < max and visited[x + 1] == -1:
         visited[x + 1] = visited[x] + 1
-        next_q.append(x + 1)
-    
-    if len(current_q) == 0:
-        current_q = next_q
-        next_q = deque()
-
+        q.append(x + 1)
 print(visited[k])
