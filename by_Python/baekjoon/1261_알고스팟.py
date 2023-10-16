6b7f3cca9f829f683b401c8b0b40e807541da8b8
donew <eastgloss0330@naver.com>
2023-10-16 17:45:21 +0900

    [백준] 1261_알고스팟 - 데크 구조 이용 - 2차원 배열 주어질 때 행과 열을 나타내는 변수 잘 확인하기

--- by_Python/baekjoon/1261_알고스팟.py
+++ by_Python/baekjoon/1261_알고스팟.py
@@ -9,10 +9,12 @@ visited = [[-1] * n for _ in range(m)] # 벽을 부순 횟수를 저장
 # BFS
 dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 visited[0][0] = 0
-current_q = deque([(0, 0)]) # 부순 벽이 없을 떄
-next_q = deque() # 부순 벽이 있을 때
-while current_q:
-    r, c = current_q.popleft()
+# 방으로 이동하는 경우는 데크의 앞에
+# 벽을 부수고 이동하는 경우는 데크의 뒤에
+q = deque([(0, 0)])
+
+while q:
+    r, c = q.popleft()
     for dir in dirs:
         new_r = r + dir[0]
         new_c = c + dir[1]
@@ -21,14 +23,10 @@ while current_q:
                 # 빈방
                 if arr[new_r][new_c] == 0:
                     visited[new_r][new_c] = visited[r][c]
-                    current_q.append((new_r, new_c))
+                    q.appendleft((new_r, new_c))
                 # 벽
                 else:
                     visited[new_r][new_c] = visited[r][c] + 1
-                    next_q.append((new_r, new_c))
-    
-    if len(current_q) == 0:
-        current_q = next_q
-        next_q = deque()
+                    q.append((new_r, new_c))
 
 print(visited[m-1][n-1])
