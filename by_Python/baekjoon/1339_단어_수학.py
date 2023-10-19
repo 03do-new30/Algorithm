64b409526b9034191956474e0283931b0ac5c22c
donew <eastgloss0330@naver.com>
2023-10-19 15:48:11 +0900

    1339_단어 수학 - 여전히 시간초과

--- by_Python/baekjoon/1339_단어_수학.py
+++ by_Python/baekjoon/1339_단어_수학.py
@@ -3,31 +3,21 @@ input = sys.stdin.readline
 
 n = int(input())
 words = []
+letters = []
 for _ in range(n):
-    words.append(input().strip())
+    word = input().strip()
+    words.append(word)
+    for x in word:
+        letters.append(x)
+letters = list(set(letters)) # 등장하는 알파벳 저장
 
-# 등장한 알파벳 개수가 k라면
-# 0-9 중 가장 큰 수 k개를 고른다
-# 브루트포스로 알파벳과 매칭해 가장 큰 합이 나오는 결과를 고른다
-
-### 1 ### 
+### 1 ###
 # 등장한 알파벳 개수 k에 저장
-alpahbets = set()
-for word in words:
-    tmp = set(word)
-    alpahbets = alpahbets.union(tmp)
-k = len(alpahbets)
+k = len(letters)
 
-# 0-9 중 가장 큰 수 k개를 고른다
+# 0-9 중 가장 큰 수 k개를 고르고, 순열로 만든 뒤 알파벳과 매칭
 nums = [x for x in range(9, 9-k, -1)]
 
-# 알파벳과 매칭해서 가장 큰 합이 나오는 결과를 고른다
-# 알파벳과 매칭하는 방법
-# 알파벳에 1:1로 대응되는 인덱스를 부여한다
-# 큰 수를 순열로 쭉 만들어보고, 순열의 인덱스와 알파벳의 인덱스가 같은 것들을 매칭
-alpahbets = sorted(list(alpahbets))
-
-# nums를 순열로 만든다
 def prev_permutation(seq):
     i = len(seq) - 1
     while i > 0 and seq[i-1] <= seq[i]:
@@ -48,24 +38,23 @@ def prev_permutation(seq):
     return True
 
 answer = 0
+
 while True:
-    word_num_dict = dict()
+    matches = dict()
     for i in range(k):
-        w = alpahbets[i]
-        n = nums[i]
-        word_num_dict[w] = n
+        ll = letters[i]
+        nn = nums[i]
+        matches[ll] = nn
     
-    sum = 0
+    tmp = 0
     for word in words:
-        if len(word) == 1:
-            sum += int(str(word_num_dict[word]))
-            continue
-        result = ''
+        word_num = ''
         for x in word:
-            result += str(word_num_dict[x])
-        sum += int(result)
+            word_num += str(matches[x])
+        word_num = int(word_num)
+        tmp += word_num
     
-    answer = max(answer, sum)
+    answer = max(answer, tmp)
 
     if not prev_permutation(nums):
         break
