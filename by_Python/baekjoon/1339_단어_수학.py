ab192f55467d9b5faf4122a3f8ed2d4e91ef95e8
donew <eastgloss0330@naver.com>
2023-10-19 16:12:22 +0900

    [백준] 1339_단어 수학 - 그리디로 풀이! 시간초과 안난다 후후

--- by_Python/baekjoon/1339_단어_수학.py
+++ by_Python/baekjoon/1339_단어_수학.py
@@ -1,62 +1,32 @@
 import sys
 input = sys.stdin.readline
 
+# 참고 출처: https://yoonsang-it.tistory.com/41
+
 n = int(input())
+
+alphabet_dict = dict()
+for i in range(65, 91):
+    alphabet_dict[chr(i)] = 0
+
 words = []
-letters = []
 for _ in range(n):
-    word = input().strip()
-    words.append(word)
-    for x in word:
-        letters.append(x)
-letters = list(set(letters)) # 등장하는 알파벳 저장
+    words.append(input().strip())
 
-### 1 ###
-# 등장한 알파벳 개수 k에 저장
-k = len(letters)
+for word in words:
+    for i in range(len(word)):
+        num = 10 ** (len(word) - i - 1)
+        let = word[i]
+        alphabet_dict[let] += num
 
-# 0-9 중 가장 큰 수 k개를 고르고, 순열로 만든 뒤 알파벳과 매칭
-nums = [x for x in range(9, 9-k, -1)]
+alphabet_list = []
+for value in alphabet_dict.values():
+    if value > 0:
+        alphabet_list.append(value)
 
-def prev_permutation(seq):
-    i = len(seq) - 1
-    while i > 0 and seq[i-1] <= seq[i]:
-        i -= 1
-    if i == 0:
-        return False
-    
-    j = len(seq) - 1
-    while seq[i-1] <= seq[j]:
-        j -= 1
-    seq[i-1], seq[j] = seq[j], seq[i-1]
-
-    j = len(seq) - 1
-    while i < j:
-        seq[i], seq[j] = seq[j], seq[i]
-        i += 1
-        j -= 1
-    return True
-
+sorted_list = sorted(alphabet_list, reverse=True)
 answer = 0
-
-while True:
-    matches = dict()
-    for i in range(k):
-        ll = letters[i]
-        nn = nums[i]
-        matches[ll] = nn
-    
-    tmp = 0
-    for word in words:
-        word_num = ''
-        for x in word:
-            word_num += str(matches[x])
-        word_num = int(word_num)
-        tmp += word_num
-    
-    answer = max(answer, tmp)
-
-    if not prev_permutation(nums):
-        break
+for i in range(len(sorted_list)):
+    answer += sorted_list[i] * (9 - i)
 
 print(answer)
