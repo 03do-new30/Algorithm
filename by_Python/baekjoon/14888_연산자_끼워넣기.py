309e1e4e821c0c3695198c79a906b2aa044849ff
donew <eastgloss0330@naver.com>
2023-10-20 13:41:27 +0900

    [백준] 14888_연산자 끼워넣기 - 순열 이용 풀이

--- by_Python/baekjoon/14888_연산자_끼워넣기.py
+++ by_Python/baekjoon/14888_연산자_끼워넣기.py
@@ -7,41 +7,48 @@ signs = list(map(int, input().split())) # +, -, *, //
 
 min_result = 10000000000
 max_result = -10000000000
-def solve(idx, result):
 
-    global min_result
-    global max_result
-
-    if idx == n-1:
-        min_result = min(min_result, result)
-        max_result = max(max_result, result)
-        return
+# 순열로 풀어보기
+# 연산자 -> 순열로 만든다
+seq = [0] * signs[0] + [1] * signs[1] + [2] * signs[2] + [3] * signs[3]
+def next_permutation(seq):
+    i = len(seq) - 1
+    while i > 0 and seq[i-1] >= seq[i]:
+        i -= 1
+    if i == 0:
+        return False
     
-    for i in range(4):
-        if signs[i] <= 0:
-            continue
-        if i == 0: # +
-            signs[i] -= 1
-            solve(idx + 1, result + a[idx+1])
-            signs[i] += 1
-        elif i == 1: # -
-            signs[i] -= 1
-            solve(idx + 1, result - a[idx+1])
-            signs[i] += 1
-        elif i == 2: # *
-            signs[i] -= 1
-            solve(idx + 1, result * a[idx+1])
-            signs[i] += 1
-        else: # //
-            signs[i] -= 1
-            # 음수를 양수로 나눌 때
-            if result < 0 and a[idx + 1] > 0:
-                tmp = -result // a[idx + 1]
-                solve(idx + 1, -tmp)
-            else:
-                solve(idx + 1, result // a[idx+1])
-            signs[i] += 1
+    j = len(seq) - 1
+    while seq[i-1] >= seq[j]:
+        j -= 1
+    seq[i-1], seq[j] = seq[j], seq[i-1]
 
-solve(0, a[0])
+    j = len(seq) - 1
+    while i < j:
+        seq[i], seq[j] = seq[j], seq[i]
+        i += 1
+        j -= 1
+    
+    return True
+
+while True:
+    result = a[0]
+    for i in range(n-1):
+        if seq[i] == 0: # +
+            result += a[i+1]
+        elif seq[i] == 1: # -
+            result -= a[i+1]
+        elif seq[i] == 2: # *
+            result *= a[i+1]
+        else: # /
+            if result < 0:
+                result = -(-result // a[i+1])
+            else:
+                result //= a[i+1]
+    min_result = min(min_result, result)
+    max_result = max(max_result, result)
+    if not next_permutation(seq):
+        break
+
 print(max_result)
 print(min_result)
