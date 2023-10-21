afb54c8cdf32c1c1c0ee83cdf37fbbaa2dd24acd
donew <eastgloss0330@naver.com>
2023-10-21 23:21:14 +0900

    [백준] 1182_부분수열의 합 - 재귀

--- by_Python/baekjoon/1182_부분수열의_합.py
+++ by_Python/baekjoon/1182_부분수열의_합.py
@@ -4,24 +4,27 @@ input = sys.stdin.readline
 n, s = map(int, input().split())
 nums = list(map(int, input().split()))
 
-# 비트마스킹을 이용해 부분집합 구하기
-# arr[i] = 부분집합 i의 합을 저장
-arr = [0] * (1 << n)
-
-for i in range(len(arr)):
-    total = 0 # 합 저장
-    
-    # j번째 수가 부분집합 i에 포함되는지 확인한다
-    for j in range(n):
-        if i & (1 << j) > 0: #포함
-            total += nums[j]
-    
-    arr[i] = total
-
-# 공집합(0)인 경우를 제외하고, 합이 S가 되는 경우를 센다
 answer = 0
-for i in range(1, len(arr)):
-    if arr[i] == s:
-        answer += 1
 
+# idx -> 부분수열에 포함해야할지 말지 결정해야하는 인덱스
+# sum -> 현재까지 부분수열의 합
+def solve(idx, sum):
+    global answer
+    
+    if idx == n and sum == s:
+        answer += 1
+        return
+    
+    if idx == n and sum != s:
+        return
+    
+    # idx번째 수를 선택하는 경우
+    solve(idx + 1, sum + nums[idx])
+    # idx번째 수를 선택하지 않는 경우
+    solve(idx + 1, sum)
+
+solve(0, 0)
+if s == 0:
+    # 공집합 제거해주기
+    answer -= 1
 print(answer)
