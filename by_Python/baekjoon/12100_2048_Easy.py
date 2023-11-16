3e59502c5215d163e0e4e1e09653d15205e00d40
donew <eastgloss0330@naver.com>
2023-11-17 00:52:42 +0900

    [백준] 12100_2048 Easy - 비트마스크

--- by_Python/baekjoon/12100_2048_Easy.py
+++ by_Python/baekjoon/12100_2048_Easy.py
@@ -1,98 +1,114 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
-board = [list(map(int, input().split())) for _ in range(N)]
+LIMIT = 5
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
+def gen(k):
+    # 이동을 4진법으로 표시
+    a = [0] * LIMIT
+    for i in range(LIMIT):
+        a[i] = k & 3
+        k = k >> 2
+    return a
 
-def move_board(board, dx, dy):
-    # return할 새로운 상태의 보드
-    new_board = [[0]*N for _ in range(N)]
+def check(arr, dirs):
+    n = len(arr)
 
-    # 합쳐졌는지 여부를 표시하는 collision
-    collision = [[False]*N for _ in range(N)]
+    for dir in dirs:
+        merged = [[False] * n for _ in range(n)]
 
-    # 위로 옮기면 가장 위 row부터,
-    # 아래로 옮길 때는 가장 아래 row부터,
-    # 왼쪽으로 옮길 때는 가장 좌측 col부터,
-    # 오른쪽으로 옮길 때는 가장 우측 col부터 움직여야 한다.
+        while True:
+            moved = False
 
-    # 위, 아래
-    if dy == 0:
-        # 위
-        if dx == -1:
-            row_range = range(N)
-        # 아래
-        elif dx == 1:
-            row_range = range(N-1, -1, -1)
+            if dir == 0:
+                for i in range(n-2, -1, -1):
+                    for j in range(n):
+                        if arr[i][j] == 0:
+                            continue
+                        if arr[i+1][j] == 0:
+                            arr[i+1][j] = arr[i][j]
+                            merged[i+1][j] = merged[i][j]
+                            arr[i][j] = 0
+                            moved = True
+                        elif arr[i+1][j] == arr[i][j]:
+                            if not merged[i][j] and not merged[i+1][j]:
+                                arr[i+1][j] *= 2
+                                merged[i+1][j] = True
+                                arr[i][j] = 0
+                                moved = True
+            elif dir == 1:
+                for i in range(1, n):
+                    for j in range(n):
+                        if arr[i][j] == 0:
+                            continue
+                        if arr[i-1][j] == 0:
+                            arr[i-1][j] = arr[i][j]
+                            merged[i-1][j] = merged[i][j]
+                            arr[i][j] = 0
+                            moved = True
+                        elif arr[i-1][j] == arr[i][j]:
+                            if not merged[i-1][j] and not merged[i][j]:
+                                arr[i-1][j] *= 2
+                                merged[i-1][j] = True
+                                arr[i][j] = 0
+                                moved = True
+            elif dir == 2:
+                for j in range(1, n):
+                    for i in range(n):
+                        if arr[i][j] == 0:
+                            continue
+                        if arr[i][j-1] == 0:
+                            arr[i][j-1] = arr[i][j]
+                            merged[i][j-1] = merged[i][j]
+                            arr[i][j] = 0
+                            moved = True
+                        elif arr[i][j-1] == arr[i][j]:
+                            if not merged[i][j] and not merged[i][j-1]:
+                                arr[i][j-1] *= 2
+                                merged[i][j-1] = True
+                                arr[i][j] = 0
+                                moved = True
+            elif dir == 3:
+                for j in range(n-2, -1, -1):
+                    for i in range(n):
+                        if arr[i][j] == 0:
+                            continue
+                        if arr[i][j+1] == 0:
+                            arr[i][j+1] = arr[i][j]
+                            merged[i][j+1] = merged[i][j]
+                            arr[i][j] = 0
+                            moved = True
+                        elif arr[i][j+1] == arr[i][j]:
+                            if not merged[i][j] and not merged[i][j+1]:
+                                arr[i][j+1] *= 2
+                                merged[i][j+1] = True
+                                arr[i][j] = 0
+                                moved = True
+            
+            if not moved:
+                break
+    
+    ans = max([max(row) for row in arr])
+    return ans
+    
 
-        for r in row_range:
-            for c in range(N):
-                if board[r][c] != 0:
-                    move_block(board, dx, dy, r, c, new_board, collision)
+n = int(input())
+original = []
+for _ in range(n):
+    original.append(list(map(int, input().split())))
 
-    # 왼쪽, 오른쪽
-    elif dx == 0:
-        # 왼쪽
-        if dy == -1:
-            col_range = range(N)
-        # 오른쪽
-        elif dy == 1:
-            col_range = range(N-1, -1, -1)
+answer = 0
 
-        for c in col_range:
-            for r in range(N):
-                if board[r][c] != 0:
-                    move_block(board, dx, dy, r, c, new_board, collision)
+# 최대 5번 이동, 경우의 수는 4**5 = 2**10 = 1024
+for k in range(1 << (LIMIT * 2)):
+    dirs = gen(k)
+    
+    arr = [row[:] for row in original]
 
-    return new_board
+    cur = check(arr, dirs)
+    if cur > answer:
+        answer = cur
 
-
-def move_block(board, dx, dy, r, c, new_board, collision):
-    new_r = r
-    new_c = c
-
-    while 0 <= new_r + dx < N and 0 <= new_c + dy < N:
-        new_r += dx
-        new_c += dy
-        # 빈칸이 아니라면 중단
-        if new_board[new_r][new_c] != 0:
-            break
-
-    while True:
-        # 숫자가 같고, (new_r, new_c)가 합쳐진 적이 없으면 합쳐짐
-        if new_board[new_r][new_c] == board[r][c] and not collision[new_r][new_c]:
-            new_board[new_r][new_c] += board[r][c]
-            # 합쳐졌음을 표시
-            collision[new_r][new_c] = True
-            break
-
-        # 빈칸을 만나면 그자리에 들어감
-        if new_board[new_r][new_c] == 0:
-            new_board[new_r][new_c] = board[r][c]
-            break
-
-        # 왔던 방향의 반대로 되돌아감
-        new_r -= dx
-        new_c -= dy
-
-
-def solve(board, cnt):
-    global max_block
-
-    moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-    if cnt == 5:
-        current_max = max(map(max, board))
-        max_block = max(max_block, current_max)
-        return
-
-    for move in moves:
-        dx, dy = move
-        new_board = move_board(board, dx, dy)
-        solve(new_board, cnt + 1)
-
-
-max_block = 0
-solve(board, 0)
-print(max_block)
+print(answer)
