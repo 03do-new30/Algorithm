deb29ebceaafe4c6e34c388ef8d5935632b1b873
donew <eastgloss0330@naver.com>
2023-11-24 16:44:49 +0900

    [백준] 2206_벽 부수고 이동하기

--- by_Python/baekjoon/2206_벽_부수고_이동하기.py
+++ by_Python/baekjoon/2206_벽_부수고_이동하기.py
@@ -1,67 +1,46 @@
-from collections import deque
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 arr = [list(map(int, list(input().strip()))) for _ in range(n)]
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
-# 3차원 배열로 표현
-# visited[r][c][0] -> 벽을 한 번도 안 뚫었을 때
-# visited[r][c][1] -> 벽을 한번이라도 뚫었을 때
-visited = [[[0] * 2 for _ in range(m)] for __ in range(n)]
+def bfs():
+    # visited[r][c][0] = 벽을 부수지 않은 상태
+    # visited[r][c][1] = 벽을 부순 상태
+    visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+    q = deque([(0, 0, 0)]) #(행, 열, 벽 부수지 않으면 0이고 부순 상태이면 1)
+    visited[0][0][0] = 1 # 시작하는 칸도 포함해서 센다
 
-# flag가 1일 때 -> 벽을 한 번이라도 뚫음
-# flag가 0일 때 -> 벽을 한 번도 안 뚫음
-
-
-def bfs(r, c, flag):
-    q = deque([])
-    q.append((r, c, flag))
-    visited[r][c][flag] = 1
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
 
     while q:
-
-        r, c, flag = q.popleft()
-
-        """
-        if flag == 0:
-            print("벽을 파괴하지 않은", end=' ')
-        else:
-            print("벽을 파괴한", end=' ')
-        print("(", r, ",", c, ")", "에서 탐색한 결과")
-        """
-
+        r, c, destroy = q.popleft()
         for i in range(4):
-            new_r = r + dx[i]
-            new_c = c + dy[i]
-
-            if 0 <= new_r < n and 0 <= new_c < m:
-                if arr[new_r][new_c] == 0:
-                    if visited[new_r][new_c][flag] == 0:
-                        visited[new_r][new_c][flag] = visited[r][c][flag] + 1
-                        q.append((new_r, new_c, flag))
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < n and 0 <= nc < m:
+                # 빈공간
+                if arr[nr][nc] == 0:
+                    if visited[nr][nc][destroy] == 0:
+                        visited[nr][nc][destroy] = visited[r][c][destroy] + 1
+                        q.append((nr, nc, destroy))
+                # 벽
                 else:
-                    if flag == 0:
-                        # print("(", new_r, ",", new_c, ") 에서 벽을 파괴")
-                        visited[new_r][new_c][1] = visited[r][c][0] + 1
-                        q.append((new_r, new_c, 1))
-        """
-        for row in visited:
-            print(row)
-        print('='*20)
-        """
+                    # 벽을 부수지 않은 상태여야 한다
+                    if destroy == 0:
+                        visited[nr][nc][1] = visited[r][c][0] + 1
+                        q.append((nr, nc, 1))
+    
+    return (visited[n-1][m-1][0], visited[n-1][m-1][1])
 
 
-# bfs 실행
-bfs(0, 0, 0)
-
-# 출력
-if 0 in visited[n-1][m-1]:
-    min_dist = max(visited[n-1][m-1][0], visited[n-1][m-1][1])
+ans = bfs()
+if 0 in ans:
+    min_dist = max(ans)
 else:
-    min_dist = min(visited[n-1][m-1][0], visited[n-1][m-1][1])
+    min_dist = min(ans)
 
 if min_dist == 0:
     print(-1)
