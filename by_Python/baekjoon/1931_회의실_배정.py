7036a4fad3317fa70e242c1480c38fd10a284afa
donew <eastgloss0330@naver.com>
2023-12-07 16:29:36 +0900

    [백준] 1931_회의실 배정

--- by_Python/baekjoon/1931_회의실_배정.py
+++ by_Python/baekjoon/1931_회의실_배정.py
@@ -2,15 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-by_total = dict()
+# "빨리 끝나는" 회의부터 배정 -> 그래야 뒤에 배정할 회의가 더 많음
+meetings = []
 for _ in range(n):
     start, end = map(int, input().split())
-    total = end - start
-    if total in by_total:
-        by_total[total].append((start, end))
-    else:
-        by_total[total] = [(start, end)]
-print(by_total)
+    meetings.append((start, end))
 
-for total in sorted(by_total.keys()):
-    print(by_total[total])
+# end를 기준으로 정렬
+meetings.sort(key=lambda x : (x[1], x[0]))
+ans = 0
+
+last_time = 0 # 가장 마지막에 끝난 회의 시간 저장
+for meeting in meetings:
+    start, end = meeting
+    if start >= last_time:
+        last_time = end
+        ans += 1
+print(ans)
