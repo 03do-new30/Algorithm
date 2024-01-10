1d5699ec2787d03f400ef066490dbda6a33c6efa
donew <eastgloss0330@naver.com>
2024-01-10 17:59:55 +0900

    상한 하한

---
+++ by_Python/ALGORITHM_TOOLS/lower_upper_bound.py
@@ -0,0 +1,40 @@
+arr = [10, 9, 2, 3, 4, 1, 7, 17, 18, 130]
+n = len(arr)
+
+# lower bound
+def lower_bound(target):
+    left = 0
+    right = n-1
+    position = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if target == arr[mid]:
+            position = mid # 하한은 target보다 작거나 같은 수 중 첫번째 수
+            # 하한을 찾기 위해 더 왼쪽을 바라봄
+            right = mid - 1
+        elif target < arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return position
+
+# upper bound
+def upper_bound(target):
+    left = 0
+    right = n-1
+    position = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if target == arr[mid]:
+            position = mid + 1 # 상한은 target보다 큰 수 중 첫번쨰 수
+            # 상한을 찾기 위해 더 오른쪽을 바라봄
+            left = mid + 1
+        elif target < arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return position
