cbc2408fe8d87680f92aa91d7c87d8bad14ff86a
donew <eastgloss0330@naver.com>
2024-02-09 17:49:55 +0900

    [백준] 2022_사다리 - 코드플러스 해설 참고 (수학)

---
+++ by_Python/baekjoon/2022_사다리.py
@@ -0,0 +1,22 @@
+import sys, math
+input = sys.stdin.readline
+
+"""
+codeplus 해설 참고
+"""
+x, y, c = map(float, input().split())
+
+left = 0
+right = min(x, y)
+
+while abs(right - left) > 1e-6:
+    mid = (left + right) / 2
+    d = mid
+    h1 = math.sqrt(x*x - d*d)
+    h2 = math.sqrt(y*y - d*d)
+    h = (h1 * h2) / (h1 + h2)
+    if h > c:
+        left = mid
+    else:
+        right = mid
+print(left)
