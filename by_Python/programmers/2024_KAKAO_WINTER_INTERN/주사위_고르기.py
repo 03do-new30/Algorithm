084ac52d36c01b6460095da7855113038786d72e
donew <eastgloss0330@naver.com>
2024-03-04 10:40:10 +0900

    [2024 KAKAO WINTER INTERNSHIP] 주사위 고르기

--- by_Python/programmers/2024_KAKAO_WINTER_INTERN/주사위_고르기.py
+++ by_Python/programmers/2024_KAKAO_WINTER_INTERN/주사위_고르기.py
@@ -1,85 +1,76 @@
 from itertools import combinations
 
-def get_scores(dice, combis):
+def get_scores(dice, combi):
+    n = len(combi)
     scores = []
-    limit = len(dice) // 2
-    for combi in combis:
-        current = []
-        for i1 in range(6):
-            tmp1 = dice[combi[0]][i1]
-            if limit == 1:
-                current.append(tmp1)
+    for i1 in range(6):
+        tmp1 = dice[combi[0]][i1]
+        if n == 1:
+            scores.append(tmp1)
+            continue
+        for i2 in range(6):
+            tmp2 = tmp1 + dice[combi[1]][i2]
+            if n == 2:
+                scores.append(tmp2)
                 continue
-            
-            for i2 in range(6):
-                tmp2 = tmp1 + dice[combi[1]][i2]
-                if limit == 2:
-                    current.append(tmp2)
+            for i3 in range(6):
+                tmp3 = tmp2 + dice[combi[2]][i3]
+                if n == 3:
+                    scores.append(tmp3)
                     continue
-                
-                for i3 in range(6):
-                    tmp3 = tmp2 +  dice[combi[2]][i3]
-                    if limit == 3:
-                        current.append(tmp3)
+                for i4 in range(6):
+                    tmp4 = tmp3 + dice[combi[3]][i4]
+                    if n == 4:
+                        scores.append(tmp4)
                         continue
-                    
-                    for i4 in range(6):
-                        tmp4 = tmp3 + dice[combi[3]][i4]
-                        if limit == 4:
-                            current.append(tmp4)
-                            continue
-                        
-                        for i5 in range(6):
-                            tmp5 = tmp4 + dice[combi[4]][i5]
-                            current.append(tmp5)
-        scores.append(current)
+                    for i5 in range(6):
+                        tmp5 = tmp4 + dice[combi[4]][i5]
+                        if n == 5:
+                            scores.append(tmp5)
     return scores
+
 
 def solution(dice):
     n = len(dice)
-    # A가 가져가는 주사위 조합
+    # A가 가져가는 주사위 조합들
     a_combis = list(combinations(range(n), n//2))
-    # B가 가져가는 주사위 조합
-    b_combis = []
+    
+    max_combi = None
+    max_cnt = 0
     for a_combi in a_combis:
-        tmp = []
-        for x in range(n):
-            if x not in a_combi:
-                tmp.append(x)
-        b_combis.append(tuple(tmp))
-    
-    # A가 가져가는 주사위 조합의 점수들
-    a_scores = get_scores(dice, a_combis)
-    # B가 가져가는 주사위 조합의 점수들
-    b_scores = get_scores(dice, b_combis)
+        # B가 가져가는 주사위 조합
+        b_combi = tuple(set(x for x in range(n)).difference(a_combi))
+        # A가 a_combi대로 주사위를 선택했을 때, 점수를 저장
+        a_scores = get_scores(dice, a_combi)
+        # B가 b_combi대로 주사위를 선택했을 때, 점수를 저장
+        b_scores = get_scores(dice, b_combi)
 
-    # a_scores와 b_scores를 비교하여, 주사위 조합별 a가 이긴 횟수를 저장한다
-    a_wins = []
-    for i in range(len(a_scores)):
-        a_win = 0
-        print("a_scores[i]:", a_scores[i])
-        print("b_scores[i]:", b_scores[i])
-        print("-" * 30)
-        for j in range(len(a_scores[i])):
-            if a_scores[i][j] > b_scores[i][j]:
-                a_win += 1
-        a_wins.append(a_win)
-    print("a_wins:", a_wins)
-    # a_wins에서 이긴 횟수가 가장 많은 인덱스번째 조합이 a가 승리할 확률이 가장 높은 주사위 조합이다
-    max_wins = max(a_wins)
-    max_idx = 0
-    for i in range(len(a_wins)):
-        if a_wins[i] == max_wins:
-            max_idx = i
-            break
-    
-    # A가 골라야 하는 주사위 번호를 오름차순으로 return
-    result = []
-    for x in a_combis[max_idx]:
-        result.append(x + 1)
-    result.sort()
-    return result
-    
+        # 오름차순으로 정렬된 a와 b의 점수들을 비교해서
+        # a_combi 조합으로 b_combi 조합을 몇번 이길 수 있는지 검사
+        a_scores.sort()
+        b_scores.sort()
+        # print("a_scores:", a_scores)
+        # print("b_scores:", b_scores)
+        # a가 b를 이기는 경우를 이분탐색으로 구한다
+        tmp = 0
+        for a_score in a_scores:
+            left = 0 # b_scores의 왼쪽 인덱스
+            right = len(b_scores) - 1 # b_scores의 오른쪽 인덱스
+            while left <= right:
+                mid = (left + right) // 2
+                if a_score <= b_scores[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # a가 b를 이기는 경우는 left
+            tmp += left
+
+        if max_cnt < tmp:
+            max_cnt = tmp
+            max_combi = [x+1 for x in a_combi]
+        
+
+    return max_combi
         
                 
 inputs = [
