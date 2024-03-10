2990a13ebc24710c1f5cd2382ed1c2a313d12bf3
donew <eastgloss0330@naver.com>
2024-03-10 15:37:04 +0900

    [2023 KAKAO BLIND] 택배 배달과 수거하기 - 스택

--- by_Python/programmers/2023_KAKAO_BLIND_RECRUITMENT/택배_배달과_수거하기.py
+++ by_Python/programmers/2023_KAKAO_BLIND_RECRUITMENT/택배_배달과_수거하기.py
@@ -1,90 +1,70 @@
 def solution(cap, n, deliveries, pickups):
-    checks = [False] * n # 완료 여부
-    dist = 0 # 총 이동한 거리
+    print("cap:", cap)
+    dist = 0
+    deli_stack = []
+    pick_stack = []
+    for i in range(n):
+        if deliveries[i] > 0:
+            deli_stack.append((i+1, deliveries[i])) #(집 번호, 배달할 택배)
+        if pickups[i] > 0:
+            pick_stack.append((i+1, pickups[i]))
     
-    while False in checks:
-        truck = 0 # 창고에서 출발하는 트럭
-
-        # 배달을 실시한 가장 먼 집의 거리
-        far_delivery = -1
-
-        # [배달] 끝집부터 검사
-        for i in range(n-1, -1, -1):
-            if deliveries[i] == 0:
-                continue
-
-            if truck >= cap:
-                break
-
-            if far_delivery < i:
-                far_delivery = i
-
-            if truck + deliveries[i] <= cap:
-                truck += deliveries[i]
-                deliveries[i] = 0
+    while len(deli_stack) > 0 or len(pick_stack) > 0:
+        truck = 0
+        # 배달 단계
+        # 현재 가야하는 가장 먼 집
+        far_deli = deli_stack[-1][0] if deli_stack else -1
+        far_pick = pick_stack[-1][0] if pick_stack else -1
+        far_house = max(far_deli, far_pick)
+        print(far_house, '번 집으로 슝슝')
+        while deli_stack and truck < cap:
+            house, box = deli_stack.pop()
+            # 배송 가능한 택배 개수
+            possible = cap - truck
+            if box <= possible:
+                truck += box
+                print(house,'번 집에', box, '개 배송')
             else:
-                possible = cap - truck
-                deliveries[i] -= possible
                 truck += possible
-            print(i+1, "번째 집에서 택배 업무 진행")
-            
-            # 완료 여부 체크
-            if deliveries[i] == 0 and pickups[i] == 0:
-                checks[i] = True
-        
-
-        back_truck = 0 # 돌아오는 트럭
-
-        # 수거를 실시한 가장 먼 집의 거리
-        far_back = -1
-
-        # [수거]
-        for i in range(n-1, -1, -1):
-            if pickups[i] == 0:
-                continue
-
-            if back_truck >= cap:
-                break
-
-            if far_back < i:
-                far_back = i
-
-            if back_truck + pickups[i] <= cap:
-                back_truck += pickups[i]
-                pickups[i] = 0
+                deli_stack.append((house, box - possible)) # 남은 택배는 스택에 다시 넣어둔다
+                print(house,'번 집에', possible, '개 배송')
+        # 수거 단계
+        back_truck = 0
+        while pick_stack and back_truck < cap:
+            house, box = pick_stack.pop()
+            # 수거 가능한 택배 개수
+            possible = cap - back_truck
+            if box <= possible:
+                back_truck += box
+                print(house,'번 집에서', box, '개 수거')
             else:
-                possible = cap - back_truck
-                pickups[i] -= possible
                 back_truck += possible
-            print(i+1, "번째 집에서 수거 업무 진행")
-            
-            # 완료 여부 체크
-            if deliveries[i] == 0 and pickups[i] == 0:
-                checks[i] = True
-        
-        print("배달한 가장 먼 집의 거리는", far_delivery + 1)
-        print("수거한 가장 먼 집의 거리는", far_back + 1)
-
-
-        dist += max(far_delivery + 1, far_back + 1) * 2
-
+                pick_stack.append((house, box - possible))
+                print(house,'번 집에서', possible, '개 수거')
         print('~' * 20)
+        dist += far_house * 2
     print("dist:", dist)
     return dist
 
 
-cap = [4, 2]
-n = [5, 7]
+cap = [4, 2, 1, 2, 3]
+n = [5, 7, 5, 2, 2]
 deliveries = [
     [1, 0, 3, 1, 2]	,
-    [1, 0, 2, 0, 1, 0, 2]	
+    [1, 0, 2, 0, 1, 0, 2],
+    [0, 0, 1, 0, 0],
+    [0, 0],
+    [2, 4]
 ]
 pickups = [
     [0, 3, 0, 4, 0],
-    [0, 2, 0, 1, 0, 2, 0]	
+    [0, 2, 0, 1, 0, 2, 0],
+    [0, 0, 0, 0, 0],
+    [0, 4],
+    [4, 2]
 ]
-result = [16, 30]
+result = [16, 30, 6, 8, 8]
 
-for i in range(2):
+for i in range(len(result)):
     print(result[i] == solution(cap[i], n[i], deliveries[i], pickups[i]))
     print('-' * 40)
